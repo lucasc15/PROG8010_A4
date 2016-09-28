f6f4d3d0cfa668d089848e61858481ba0084141f
Lucas Currah <Lucas Currah>
2016-09-28 19:41:44 -0400

    Working assignment 4

---
+++ TimeCalculator/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel vm = new ViewModel();
+        // KeyConverter kc = new KeyConverter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void btnCalculateClick(object sender, RoutedEventArgs e)
+        {
+            vm.ConvertTime();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int _;
+            TextBox txtBox = (TextBox)sender;
+            for (var i=0; i<txtBox.Text.Length; i++)
+            {
+                if (!int.TryParse(txtBox.Text[i].ToString(), out _))
+                {
+                    txtBox.Text = txtBox.Text.Remove(i, 1);
+                }
+            }
+        }
+    }
+}
---
+++ TimeCalculator/ViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCalculator
+{
+    class ViewModel : ObservableObject
+    {
+        private float _secsInMinute = (float)60;
+        private float _secsInHour = (float)60 * 60;
+        private float _secsInDay = (float)60 * 60 * 24;
+        private string _seconds;
+        private float tol = 0.004f;
+        public string seconds
+        {
+            get { return _seconds; }
+            set { _seconds = value; OnPropertyChanged(); }
+        }
+
+        private string _convertedStr;
+        public string convertedStr
+        {
+            get { return _convertedStr; }
+            set { _convertedStr = value; OnPropertyChanged(); }
+        }
+
+        public void ConvertTime()
+        {
+            float secondsFloat;
+            string convertedUnits;
+            float convertedTime;
+            Debug.Print(_seconds);
+            if (_seconds != null){
+                secondsFloat = float.Parse(_seconds);
+            } else
+            {
+                secondsFloat = (float)0;
+            }
+            if (secondsFloat < _secsInMinute)
+            {
+                convertedUnits = "seconds";
+                convertedTime = secondsFloat;
+            } else if (secondsFloat < _secsInHour)
+            {
+                convertedUnits = "minutes";
+                convertedTime = secondsFloat / _secsInMinute;
+            } else if (secondsFloat < _secsInDay)
+            {
+                convertedUnits = "hours";
+                convertedTime = secondsFloat / _secsInHour;
+            } else
+            {
+                convertedUnits = "days";
+                convertedTime = secondsFloat / _secsInDay;
+            }
+            formatString(convertedTime, convertedUnits);
+        }
+        private void formatString(float convertedTime, string convertedUnits)
+        {
+            if (convertedTime - Math.Floor(convertedTime) < tol)
+            {
+                convertedUnits = convertedUnits.Substring(0, convertedUnits.Length - 1);
+            }
+            convertedStr = decimal.Round((decimal)convertedTime, 2).ToString("G") + " " + convertedUnits;
+        }
+    }
+}
